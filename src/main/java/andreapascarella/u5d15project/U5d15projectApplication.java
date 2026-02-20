d000092c1ef841730529cce424ef1b9f4149a045
Andrea <andreapascarella99@gmail.com>
2026-02-20 09:15:21 +0100

    added base

---
+++ src/main/java/andreapascarella/u5d15project/U5d15projectApplication.java
@@ -0,0 +1,13 @@
+package andreapascarella.u5d15project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d15projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d15projectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreapascarella/u5d15project/U5d15projectApplicationTests.java
@@ -0,0 +1,13 @@
+package andreapascarella.u5d15project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d15projectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
